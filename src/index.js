13f9baea02ed327eaa27b214b20e9d5ee580a473
EduardoSCosta <edu_costaaraujo@hotmail.com>
2022-03-15 14:37:19 -0300

    create colors settings

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './styles/settings/_colors.css';
 import './styles/generic/_box-sizing.css';
 import './styles/generic/_resets.css';
 import './styles/elements/_body.css';
