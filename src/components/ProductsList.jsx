3b71d4232b6d9843c615d3f12607be9e341971d1
EduardoSCosta <edu_costaaraujo@hotmail.com>
2022-03-15 16:56:53 -0300

    create product list styles

--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -1,14 +1,17 @@
 import Product from './Product';
+import '../styles/components/_productsList.css';
 
 const ProductsList = ({ products, addToCart }) => {
   return (
-    <div className='products-list'>
-      <h2>Products List</h2>
-      {products.map((product) => {
-        return (
-          <Product key={product.id} product={product} addToCart={() => addToCart(product)} />
-        );
-      })}
+    <div className='page'>
+      <h2 className='page__title'>Products List</h2>
+      <div className='products-list'>
+        {products.map((product) => {
+          return (
+            <Product key={product.id} product={product} addToCart={() => addToCart(product)} />
+          );
+        })}
+      </div>
     </div>
   );
 }
