c51e6fc0a4408968c09c10aeda451ebebb44b06a
EduardoSCosta <edu_costaaraujo@hotmail.com>
2022-03-16 13:02:41 -0300

    create Product component details and tests

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,6 +1,14 @@
+import '../styles/components/_product.css';
+
 const Product = ({product, addToCart}) => {
   return (
-    <p>product placeholder</p>
+    <div className='product'>
+      <p className='product__name'>{product.name}</p>
+      <p className='product__description'>{product.description}</p>
+      <button className='button product__add-to-cart-button' type='button' onClick={addToCart}>
+        Add to cart
+      </button>
+    </div>
   );
 }
 
