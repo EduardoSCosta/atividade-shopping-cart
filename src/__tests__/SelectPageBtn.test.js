e1e56a2af90f4ab8c3bc09bf31f738ccf5f2cde8
EduardoSCosta <edu_costaaraujo@hotmail.com>
2022-03-17 12:14:01 -0300

    create tests for SelectPageBtn component

---
+++ src/__tests__/SelectPageBtn.test.js
@@ -0,0 +1,18 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import SelectPageBtn from '../components/SelectPageBtn';
+
+describe('<SelectPageBtn />', () => {
+  it('should call the select page function', () => {
+    const pageName = 'Test page';
+    const selectPageFunction = jest.fn();
+
+    render(<SelectPageBtn pageName={pageName} setCurrentPage={selectPageFunction}/>);
+
+    const selectPageButton = screen.getByRole('button', {name: pageName});
+
+    userEvent.click(selectPageButton);
+    
+    expect(selectPageFunction).toHaveBeenCalled();
+  });
+});
