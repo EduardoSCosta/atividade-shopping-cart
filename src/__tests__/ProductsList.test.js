2030f908d491e3a01b271d12b1fbdc3c174dd8f9
EduardoSCosta <edu_costaaraujo@hotmail.com>
2022-03-17 12:12:28 -0300

    create tests for ProductsList component

---
+++ src/__tests__/ProductsList.test.js
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react';
+import ProductsList from '../components/ProductsList';
+
+describe('<ProductsList />', () => {
+  const products = [
+    { id: 1, name: 'first test product', description: 'first test product description' },
+    { id: 2, name: 'second test product', description: 'second test product description' }
+  ];
+
+  it('should render all products passed in props', () => {
+    render(<ProductsList products={products}/>);
+
+    const productsElements = screen.getAllByTestId('product');
+    
+    expect(productsElements).toHaveLength(2);
+  });
+});
