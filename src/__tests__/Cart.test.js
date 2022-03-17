445fd724f38eb944da02f24dd1c730b77c67c182
EduardoSCosta <edu_costaaraujo@hotmail.com>
2022-03-17 12:12:49 -0300

    create tests for Cart component

---
+++ src/__tests__/Cart.test.js
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react';
+import Cart from '../components/Cart';
+
+describe('<Cart />', () => {
+  const cartItems = [
+    { id: 1, name: 'first test product', description: 'first test product description' },
+    { id: 2, name: 'second test product', description: 'second test product description' }
+  ];
+
+  it('should render all items passed in props', () => {
+    render(<Cart cartItems={cartItems}/>);
+
+    const cartItemsElements = screen.getAllByTestId('cart-item');
+    
+    expect(cartItemsElements).toHaveLength(2);
+  });
+});
--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -2,7 +2,7 @@ import '../styles/components/_cartItem.css';
 
 const CartItem = ({ cartItem, removeFromCart }) => {
   return (
-    <div className='cart-item'>
+    <div className='cart-item' data-testid='cart-item'>
       <p className='cart-item__name'>{cartItem.name}</p>
       <p className='cart-item__description'>{cartItem.description}</p>
       <button className='button cart-item__remove-from-cart-button' type='button' onClick={removeFromCart}>
