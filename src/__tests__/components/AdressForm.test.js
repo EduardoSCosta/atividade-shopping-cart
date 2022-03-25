1aaa9ca4be6bc6f624a9bf20792e59c3439e3cfd
EduardoSCosta <edu_costaaraujo@hotmail.com>
2022-03-25 15:31:24 -0300

    create checkout form inputs and buttons tests

--- src/__tests__/components/AdressForm.test.js
+++ src/__tests__/components/AdressForm.test.js
@@ -1,15 +1,27 @@
 import { render, screen } from '@testing-library/react';
 import AddressForm from '../../components/Checkout/AddressForm';
-import countryAndStatesData from '../../data/countries_states.json';
 
 describe('<AdressForm />', () => {
   describe('when the component is rendered', () => {
+    const formValues = {
+      firstName: '',
+      lastName: '',
+      address1: '',
+      address2: '',
+      zipCode: '',
+      city: '',
+      country: '',
+      region: '',
+      rememberAddress: false,
+      nameOnCard: '',
+      cardNumber: '',
+      expiryDate: '',
+      rememberCard: false,
+      cvv: ''
+    }
+
     it('should show an empty select country field', () => {
-      render(
-        <AddressForm
-          selectedState={''}
-          selectedCountry={''}
-        />);
+      render(<AddressForm formValues={formValues} />);
 
       const selectCountryElement = screen.getByRole('button', { name: /country ​/i });
 
@@ -17,11 +29,7 @@ describe('<AdressForm />', () => {
     });
 
     it('should show an empty select state field', () => {
-      render(
-        <AddressForm
-          selectedState={''}
-          selectedCountry={''}
-        />);
+      render(<AddressForm formValues={formValues} />);
 
       const selectStateElement = screen.getByRole('button', { name: /state\/province\/region ​/i });
 
