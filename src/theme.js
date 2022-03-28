a40fd30cfcf9ad91ee3b3736549b25e61cf3f38e
EduardoSCosta <edu_costaaraujo@hotmail.com>
2022-03-28 16:20:58 -0300

    create styled-components theme

---
+++ src/theme.js
@@ -0,0 +1,37 @@
+const colors = {
+  white: '#ffffff',
+  black: '#000000',
+  blue300: '#1976d2',
+  gray100: '#e5e5e5',
+  gray200: '#e0e0e0',
+  gray300: '#9e9e9e',
+  gray400: '#8a8a8a',
+  gray500: '#676767'
+}
+
+const spaces = [0, 2, 4, 8, 16, 32, 64];
+
+const fonts = '"Roboto","Helvetica","Arial",sans-serif';
+
+const fontSizes = [8, 12, 16, 20, 24, 28, 32];
+
+const fontWeights = [400, 500, 600, 700];
+
+const radii = [0, 5, 10, 15, 20, 25, 30];
+
+const zIndices = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+
+const sizes = [50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600];
+
+const theme = {
+  colors,
+  spaces,
+  fonts,
+  fontSizes,
+  fontWeights,
+  radii,
+  zIndices,
+  sizes
+}
+
+export default theme;
