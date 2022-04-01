73b021ae98062ab8d8e2d1bbc2f2ed734d091c15
EduardoSCosta <edu_costaaraujo@hotmail.com>
2022-04-01 11:56:17 -0300

    update header buttons to use styled components

--- src/components/SelectPageBtn.jsx
+++ src/components/SelectPageBtn.jsx
@@ -1,10 +1,18 @@
+import { css } from '@styled-system/css';
+import { Button } from '../components';
 import '../styles/components/_selectPageBtn.css'
 
 const SelectPageButton = ({pageName, setCurrentPage}) => {
   return (
-    <button className='button button--select-page' type='button' onClick={() => setCurrentPage(pageName)}>
+    <Button variant='green' type='button' onClick={() => setCurrentPage(pageName)}
+    css={css({
+      backgroundColor: 'inherit',
+      height: '100%',
+      width: '150px',
+      borderRadius: '0'
+    })}>
       {pageName}
-    </button>
+    </Button>
   );
 }
 
